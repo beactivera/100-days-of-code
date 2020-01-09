bed280c015dfd6ee2452fae99d1a4aaaf275b934
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2020-01-09 21:14:43 +0100

    added pseudo code

---
+++ day 8/main.js
@@ -0,0 +1,17 @@
+/* PSEUDO CODE
+
+	1. Get the text from HTML - plus length
+	2. Remove/clear the text in the HTML
+    3. Separate intro individual characters
+    4. start with N=0
+        if N < length
+	5. Display the first character
+	6. Play sound
+        If(character == space)      play   'typespace'
+                        uppercase   play    typekey1
+                        lowercase   play    typekey2
+    7. make N one bigger that it is - make a loop back to 4 BUT remember about delay
+    8. only repeat if more characters
+    
+    
+*/

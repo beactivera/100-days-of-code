e725b0362723f66db03ef2ea7caf4ceaa8e281e3
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2020-01-09 21:26:45 +0100

    added loops and organised code using comment - pseudo code

--- day 8/main.js
+++ day 8/main.js
@@ -15,3 +15,48 @@
     
     
 */
+
+// make many times loop()  once by time
+
+//CODING
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+let text;
+let N;
+
+function init() {
+  //   console.log("init runs");
+  // get text from html
+  text = document.querySelector("#typewriter").textContent;
+  console.log(text);
+
+  // start with N=0
+  N = 0;
+
+  // start the loop
+  if (N < text.length) {
+    loop();
+  } else {
+    // stop looping
+  }
+}
+
+function loop() {
+  //   console.log("loop runs");
+  // display the Nth character;
+  setInterval(showChar(), 2000);
+  function showChar() {
+    for (let i = 0; i < text.length; i++) {
+      console.log(text.charAt([i]));
+    }
+  }
+
+  const newText = text.substring(0, N);
+  document.querySelector("#typewriter").textContant = newText;
+
+  // // next N
+  // N++;
+  console.log(N);
+}
